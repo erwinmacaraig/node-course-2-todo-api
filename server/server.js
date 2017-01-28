c68676006844e0b7269f1c3f15cbac950fc35683
Erwin Macaraig <erwin.macaraig@gmail.com>
2017-01-28 11:17:10 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,28 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+//GET /todos/1234567
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //Validate ID using isValid
+  //if not valid respond with 404
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  //Query DB using findById
+  //if success, if there is todo, send it back else send back 404
+  //if error, send back 400
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 app.listen(3000, () => {
